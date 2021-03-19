93e4435fa6a94adbd678a0529f9d89a62ee16492
Casey Deriso <caseyderiso@gmail.com>
2021-03-19 11:34:32 -0500

    created add friend mutation, connected to button on public profile pages

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -5,8 +5,9 @@ import FriendList from "../components/FriendList";
 
 import { Redirect, useParams } from "react-router-dom";
 
-import { useQuery } from "@apollo/react-hooks";
+import { useQuery, useMutation } from "@apollo/react-hooks";
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
+import { ADD_FRIEND } from "../utils/mutations";
 
 import Auth from "../utils/auth";
 
@@ -16,6 +17,8 @@ const Profile = () => {
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam },
   });
+
+  const [addFriend] = useMutation(ADD_FRIEND);
 
   const user = data?.me || data?.user || {};
 
@@ -31,17 +34,33 @@ const Profile = () => {
   if (!user?.username) {
     return (
       <h4>
-        You need to be logged in to see this page. Use the navigation links to login or signup!
+        You need to be logged in to see this page. Use the navigation links to
+        login or signup!
       </h4>
-    )
+    );
   }
+
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {userParam ? `${user.username}'s` : "your" } profile.
+          Viewing {userParam ? `${user.username}'s` : "your"} profile.
         </h2>
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
